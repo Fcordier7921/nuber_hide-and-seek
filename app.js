084704bd10029288fec2088ea510481946d60c4f
Fcordier7921 <fredericcordierpro6@gmail.com>
2021-10-14 10:15:43 +0200

    first commit

--- app.js
+++ app.js
@@ -1,62 +1,37 @@
-const inpunt = document.querySelector('#prix');
-const error = document.querySelector('small');
-const formulaire = document.querySelector('#formulaire');
-const instructions = document.querySelector('#instructions');
-let coup = 0;
-let muberP;
-let nombreAléatoire = Math.floor(Math.random() * Math.floor(1000));
-console.log(nombreAléatoire);
+let btn = document.querySelector('#changer');
+let ville = document.querySelector('#ville');
+let température = document.querySelector('#temperature_label');
 
-function verifier(nobure) {
-    let instructions = document.createElement('div');
+let villeDefaut = 'paris';
+apiMeteo(villeDefaut);
 
-    if (nobure < nombreAléatoire) {
-        instructions.textContent = `#${coup} (${nobure}) c'est plus`;
-        instructions.className = " instruction plus";
+btn.addEventListener('click', () => {
 
-    } else if (nobure > nombreAléatoire) {
-        instructions.textContent = `#${coup} (${nobure}) c'est moins`;
-
-        instructions.className = " instruction moins";
+    let changeVille = prompt('Entré le nom de la ville !');
+    villeDefaut = changeVille.replaceAll(' ', '-');
+    console.log(villeDefaut);
+    apiMeteo(villeDefaut);
 
 
-    } else {
-        instructions.textContent = `#${coup} (${nobure}) c'est le bon nombre !`;
-        instructions.className = " instruction fini";
-        inpunt.style.display = "none";
-        let button = document.querySelector('button');
-        button.innerHTML = "<button type='submit' class='btn btn-primary'>recommencer</button>";
-        button.addEventListener('click', () => {
-            location.reload();
+
+})
+
+function apiMeteo(villeDefaut) {
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + villeDefaut + "&appid=66fef5e558f25f18da4839afa6a52ee9&units=metric";
+    fetch(url) //fetch en premier argumetnl'url de l'api qui retoune un promesse
+        .then(reponse => reponse.json()) //on utilise json retour une promesse des donné
+        .then(data => { //on retrouve nos donné
+
+            if (data.message === "city not found") {
+                alert("Un probléme et intervenue, la ville non trouver! " +
+                    "revener plutard")
+            } else {
+                let VilleChoisie = data.name
+                let tempVille = data.main.temp
+                ville.textContent = VilleChoisie;
+                température.textContent = tempVille;
+            }
+
+
         })
-
-
-    }
-    document.querySelector('#instructions').prepend(instructions);
-
 }
-
-error.style.display = 'none';
-
-
-inpunt.addEventListener('keyup', () => {
-    if (isNaN(inpunt.value)) {
-        error.style.display = 'inline';
-    } else {
-        error.style.display = 'none';
-    }
-})
-
-formulaire.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    if (isNaN(inpunt.value) || inpunt.value == '') {
-        inpunt.style.borderColor = 'red';
-    } else {
-        coup++;
-        inpunt.style.border = 'silver';
-        muberP = inpunt.value;
-        inpunt.value = '';
-        verifier(muberP)
-    }
-})
